661cf379287cd17d9f1ce8827d0a2a7d14a1326b
Jinho <devjinho@gmail.com>
2014-09-11 12:27:19 +0800

    sketchy merged project files

---
+++ icehockeyWA/icehockeyWA/Assist.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace icehockeyWA
+{
+    public partial class Assist : PhoneApplicationPage
+    {
+        public Assist()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+			NavigationService.Navigate(new Uri("/PlayerNumber.xaml", UriKind.Relative));
+        }
+
+        private void NextBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+			NavigationService.Navigate(new Uri("/Game.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ icehockeyWA/icehockeyWA/Game.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace icehockeyWA
+{
+    public partial class Game : PhoneApplicationPage
+    {
+        // Constructor
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void LeftShotBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+        }
+		
+		 private void LeftGoalBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+			NavigationService.Navigate(new Uri("/PlayerNumber.xaml", UriKind.Relative));
+        }
+		
+		private void LeftPenaltyBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+			NavigationService.Navigate(new Uri("/Penalty.xaml", UriKind.Relative));
+        }
+		
+        private void RightShotBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+        }
+
+        private void RightGoalBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+			NavigationService.Navigate(new Uri("/PlayerNumber.xaml", UriKind.Relative));
+        }
+
+        private void RIghtPenaltyBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+			NavigationService.Navigate(new Uri("/Penalty.xaml", UriKind.Relative));
+        }
+		
+		private void OthersBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+			NavigationService.Navigate(new Uri("/OtherSettings.xaml", UriKind.Relative));
+        }
+    }
+}
