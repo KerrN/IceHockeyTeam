79da74e73fcdc1316b08e3f708198542d26132de
Josh <wozaski@gmail.com>
2014-12-07 23:46:49 +0800

    Added unfinished edit log and Author (me)
    Author of certain original pages, before GUI team made them look far
    superior

---
+++ icehockeyWA/icehockeyWA/Views/EditLog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+// JL unfinished
+
+namespace icehockeyWA.Views
+{
+    public partial class EditLog : PhoneApplicationPage
+    {
+        public EditLog()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //get selected details
+            //populate time dropdown // actual is selected
+            //populate type dropdown // actual is selected
+
+        }
+
+        private void confirmBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // write back to relevant event.. or change type/time or delete
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
